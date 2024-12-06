56e31141a2b6da2a48fd08b2c0bfc81f6072fdc1
nirob019207 <nirobhasanbd01955@gmial.com>
2024-12-06 22:46:07 +0600

    dk

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,7 @@ module.exports = {
         'green-title': '#749B3F',
         'image_bac': '#F4F6F6',
       },
-      fontFamily: {
-        primary: ['Montserrat', 'sans-serif', ...defaultTheme.fontFamily.sans], // Adding Montserrat as the primary font
-      },
+     
       container: {
         center: true, // Center the container
         padding: '1rem', // Apply 1rem padding on all screens
